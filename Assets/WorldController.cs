6d28bdbfb23a3430909da02cab9e121996dcc312
Павел Зубарев <pavel.zubarev@playrix.com>
2024-04-13 17:04:42 +0200

    Add game state

--- Assets/WorldController.cs
+++ Assets/WorldController.cs
@@ -6,31 +6,39 @@ using UnityEngine.Events;
 
 public class WorldController : MonoBehaviour
 {
+    private enum GameState {
+        Start,
+        Moving,
+        Fight
+    }
+
     public UnityEvent startMove;
     public UnityEvent endMove;
     public float stepTime = 0.5f;
     public float trashHold = 0.1f;
     public bool isMoving {
-        get{ return timer > 0.0f; }
+        get{ return _timer > 0.0f; }
     }
 
-    private float timer = 0.0f;
+    private float _timer = 0.0f;
+    private GameState _currentState = GameState.Start;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _currentState = GameState.Moving;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void StartUpdate() { 
+    }
+
+    void MoveUpdate() {
         bool oldIsMoving = isMoving;
         bool spacePressed = Input.GetKeyDown("space");
-        timer = Math.Max(0, timer - Time.deltaTime);
+        _timer = Math.Max(0, _timer - Time.deltaTime);
 
-        if (timer < trashHold && spacePressed) {
-            timer = stepTime;
+        if (_timer < trashHold && spacePressed) {
+            _timer = stepTime;
         }
 
         if (oldIsMoving != isMoving) {
@@ -40,6 +48,25 @@ public class WorldController : MonoBehaviour
             else {
                 endMove.Invoke();
             }
+        }
+    }
+
+    void FigthUpdate() {
+    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        switch (_currentState) {
+            case GameState.Start:
+                StartUpdate();
+                break;
+            case GameState.Moving:
+                MoveUpdate();
+                break;
+            case GameState.Fight:
+                FigthUpdate();
+                break;
         }
     }
 }
